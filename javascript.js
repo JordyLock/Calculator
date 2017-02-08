6dabc947044ababc9db442a4b2e3942099e7cdf1
DESKTOP-9RO9TVF\jordy <99039425@mydavinci.nl>
2017-02-08 11:01:51 +0100

    comments erbij gezet

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-
+//Dit zorgt ervoor dat de variabele register een waarde heeft
 var register = document.getElementById('register');
-
+//Deze functie zorgt ervoor dat de register de waarde kan vinden in de HTML.
 function test(event) {
 	console.log(event);
 register.innerHTML = register.innerHTML + event.target.innerHTML;
@@ -16,16 +16,12 @@ register.innerHTML = register.innerHTML + event.target.innerHTML;
 	document.getElementById("btn8").addEventListener("click", test);
 	document.getElementById("btn9").addEventListener("click", test);
 	document.getElementById("btn0").addEventListener("click", test);
-	// document.getElementById("btn-").addEventListener("click", test);
-	// document.getElementById("btn/").addEventListener("click", test);
-	// document.getElementById("btnx").addEventListener("click", test);
-	document.getElementById("btn.").addEventListener("click", test);
 
 
 var getal1 = register.innerHTML;
 var status = "0";
 
-
+//Dit zorgt ervoor dat je het plus knopje kan inklikken op de pagina en dat hij de cijfer ervoor onthoudt
 function plus() {
     status = "1";
  getal1 = register.innerHTML;
@@ -41,7 +37,7 @@ function plus() {
     
 console.log(getal1);
  }
-
+//Dit zorgt ervoor dat je het min knopje kan inklikken op de pagina en dat hij de cijfer ervoor onthoudt
 function minus() {
 	status = "2";
 getal1 = register.innerHTML;
@@ -53,7 +49,7 @@ getal1 = register.innerHTML;
 
 	console.log(getal1);
 }
-
+//Dit zorgt ervoor dat je het deel knopje kan gebruiken en op de pagina het cijfer ervoor onthoudt.
 function divide() {
 	status = "3";
 getal1 = register.innerHTML;
@@ -61,7 +57,7 @@ getal1 = register.innerHTML;
 op = "/";
 
 	register.innerHTML = "";
-
+//Dit zorgt ervoor dat je het keer knopje kan gebruiken en getal1 onthoudt.
 }
 function multiply() {
 	status = "4";
@@ -75,7 +71,8 @@ function multiply() {
 
 
 
-
+//Hiermee kan je de som uitrekenen door eerst te kijken welke status je hebt ingeklikt toen je op de operator klikte. 
+//en daarna kijkt hij hoe je het uit moet rekenen en laat de uitkomst in de register zien.
  function is(){
 
 console.log(status + " status");
